90a94e143003a9a5665460d57d9d2a5d4fa76a5e
Daniel Mrva <mrva.paints@gmail.com>
2022-07-08 21:12:18 -0600

    have a background gradient generated, need to work on making it full page, also figure out how to programatically change colors, etc

--- src/backgrounds/BackgroundOne.js
+++ src/backgrounds/BackgroundOne.js
@@ -1,9 +1,19 @@
 import React from "react";
 
+const styles = {
+    gradient: {
+        background: 'linear-gradient(0.25turn, #3f87a6, #ebf8e1, #f69d3c)',
+        // -webkit-background-size: 'cover',
+        // -moz-background-size: 'cover',
+        // -o-background-size: 'cover',
+        backgroundSize: 'cover',
+    }
+}
+
 const BackgroundOne = () => {
     return (
-        <div className="background-one">
-
+        <div className="background-one background-cover" style={styles.gradient}>
+            <h1>This is text</h1>
         </div>
     )
 }
