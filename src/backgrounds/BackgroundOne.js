1ef55cf256a47fa091b249834362dce9abd8cf91
Daniel Mrva <mrva.paints@gmail.com>
2022-07-11 20:46:04 -0600

    need to figure out graident stops

--- src/backgrounds/BackgroundOne.js
+++ src/backgrounds/BackgroundOne.js
@@ -2,13 +2,12 @@ import React from "react";
 
 const styles = {
     gradient: {
-        background: 'linear-gradient(0.25turn, #3f87a6, #ebf8e1, #f69d3c)',
+        background: 'linear-gradient(15deg, rgba(63,135,166,1) 0 33%, rgba(235,248,225,1) 0 66%, rgba(246,157,60,1)) 0 100%',
         // -webkit-background-size: 'cover',
         // -moz-background-size: 'cover',
         // -o-background-size: 'cover',
         backgroundSize: 'cover',
-        height: '100%'
-
+        height: '100vh'
     }
 }
 
