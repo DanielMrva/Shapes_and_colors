4d334eba438fb97b19f78768995f92177efa429a
Daniel Mrva <mrva.paints@gmail.com>
2022-07-12 21:55:15 -0600

    extra ) messed up the linear gradient thing I was doing with the stops.  Now I can move on.

--- src/backgrounds/BackgroundOne.js
+++ src/backgrounds/BackgroundOne.js
@@ -2,7 +2,9 @@ import React from "react";
 
 const styles = {
     gradient: {
-        background: 'linear-gradient(15deg, rgba(63,135,166,1) 0 33%, rgba(235,248,225,1) 0 66%, rgba(246,157,60,1)) 0 100%',
+        background: `linear-gradient(to right, rgba(63,135,166,1) 33% , rgba(235,248,225,1) 33% 66%, rgba(246,157,60,1) 66% 100%
+        `,
+        // background: 'linear-gradient(red 0%, orange 25%, yellow 50%, green 75%, blue 100%)',
         // -webkit-background-size: 'cover',
         // -moz-background-size: 'cover',
         // -o-background-size: 'cover',
